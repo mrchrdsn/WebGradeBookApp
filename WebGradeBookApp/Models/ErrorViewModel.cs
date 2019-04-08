8a940fa8535955059e03c3f3409472ff9e95fa48
Monte Richardson <monte_richardson@hotmail.com>
2019-04-08 12:44:18 -0500

    Initial check-in after project creation.

---
+++ WebGradeBookApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebGradeBookApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
